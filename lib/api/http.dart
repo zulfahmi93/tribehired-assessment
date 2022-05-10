702206911502b8bbcdcffee0432d3b10fd026e8b
Zulfahmi Ahmad <zulfahmi93@outlook.com>
2022-05-10 13:20:19 +0800

    Added logging to the http client.

--- lib/api/http.dart
+++ lib/api/http.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:logging/logging.dart';
 
 @singleton
 class HttpClient {
+  // ------------------------------- CONSTRUCTORS ------------------------------
+  HttpClient() {
+    _dio.interceptors.add(_LoggerInterceptor());
+  }
+
   // -------------------------------- CONSTANTS --------------------------------
   static const _connectTimeout = 120000;
   static const _receiveTimeout = 120000;
@@ -25,7 +33,7 @@ class HttpClient {
     TRequest? data,
     Map<String, dynamic>? queryParameters,
   }) {
-    return _dio.request(
+    return _dio.request<TResponse>(
       path,
       data: data,
       queryParameters: queryParameters,
@@ -35,5 +43,117 @@ class HttpClient {
   @disposeMethod
   void dispose() {
     _dio.close(force: true);
+  }
+}
+
+class _LoggerInterceptor extends Interceptor {
+  // ------------------------------- METHODS ------------------------------
+  @override
+  void onRequest(
+    RequestOptions options,
+    RequestInterceptorHandler handler,
+  ) async {
+    if (!Logger.root.isLoggable(Level.FINER)) {
+      handler.next(options);
+      return;
+    }
+
+    Logger.root.finer('');
+    Logger.root.finer('Sending request to ${options.uri}:');
+    Logger.root.finer('');
+
+    if (options.headers.isNotEmpty) {
+      Logger.root.finer('Request headers: ');
+      options.headers.forEach(
+        (key, value) => Logger.root.finer('$key: ${value.toString()}'),
+      );
+      Logger.root.finer('');
+    }
+
+    if (options.data != null) {
+      _printObjectInJson(options.data, 'Request data:');
+      _printFormDataObject(options.data, 'Request data:');
+      Logger.root.finer('');
+    }
+
+    handler.next(options);
+  }
+
+  @override
+  void onResponse(
+    Response response,
+    ResponseInterceptorHandler handler,
+  ) async {
+    if (!Logger.root.isLoggable(Level.FINER)) {
+      handler.next(response);
+      return;
+    }
+
+    final requestUri = response.requestOptions.uri;
+    Logger.root.finer('');
+    _printObjectInJson(response.data, '$requestUri response:');
+    Logger.root.finer('');
+
+    handler.next(response);
+  }
+
+  @override
+  void onError(
+    DioError err,
+    ErrorInterceptorHandler handler,
+  ) async {
+    if (!Logger.root.isLoggable(Level.WARNING)) {
+      handler.next(err);
+      return;
+    }
+
+    final request = err.requestOptions;
+    final response = err.response;
+
+    Logger.root.warning('');
+    Logger.root.warning('Error sending request to ${request.uri}!');
+    if (response != null) {
+      if (response.statusCode != null) {
+        Logger.root.warning('HTTP Status Code: ${response.statusCode}');
+      }
+      final msg = response.statusMessage;
+      if (msg == null || msg.isEmpty) {
+        Logger.root.warning('HTTP Status Code: $msg');
+      }
+    }
+    Logger.root.warning('');
+
+    handler.next(err);
+  }
+
+  void _printObjectInJson(dynamic obj, String log) {
+    if (obj is FormData) {
+      return;
+    }
+
+    const encoder = JsonEncoder.withIndent('  ');
+    final json = encoder.convert(obj);
+    final lines = LineSplitter.split(json).toList();
+
+    Logger.root.finer(log);
+    Logger.root.finer('');
+    for (final line in lines) {
+      Logger.root.finer(line);
+    }
+  }
+
+  void _printFormDataObject(dynamic obj, String log) {
+    if (obj is! FormData) {
+      return;
+    }
+
+    Logger.root.finer(log);
+    Logger.root.finer('');
+    for (final file in obj.files) {
+      Logger.root.finer('[File] ${file.key}: ${file.value.filename}');
+    }
+    for (final field in obj.fields) {
+      Logger.root.finer('[Field] ${field.key}: ${field.value}');
+    }
   }
 }
--- lib/di/di.dart
+++ lib/di/di.dart
@@ -1,9 +1,16 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
+import 'package:logging/logging.dart';
 
 import 'di.config.dart';
 
 final getIt = GetIt.instance;
 
 @InjectableInit()
-void configureDependencies() => $initGetIt(getIt);
+void configureDependencies() {
+  $initGetIt(getIt);
+  Logger.root.level = Level.ALL;
+  Logger.root.onRecord.listen(
+    (log) => print('${log.level.name}: ${log.time}: ${log.message}'),
+  );
+}
